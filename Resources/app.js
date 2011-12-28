79237f4c03994d9205ce6fecf14aad4486d93d2e
Marcel Pociot <m.pociot@gmail.com>
2011-12-28 15:25:16 +0100

    improved the slide functionality. It's now possible to slide using touchmove

--- Resources/app.js
+++ Resources/app.js
@@ -1,34 +1,75 @@
+// Facebook like menu window
+var menu	= Ti.UI.createWindow({
+	backgroundColor: 'red',
+	top:   0,
+	left:  0,
+	width: 250,
+	zIndex: 1
+});
+var data = [{title:"Row 1"},{title:"Row 2"},{title:"Row 3"},{title:"Row 4"}];
+var tableView	= Ti.UI.createTableView({ data: data });
+menu.add(tableView);
+menu.open();
+
 // animations
 var animateLeft	= Ti.UI.createAnimation({
 	left: 250,
+	curve: Ti.UI.iOS.ANIMATION_CURVE_EASE_OUT,
 	duration: 500
 });
 var animateRight	= Ti.UI.createAnimation({
 	left: 0,
+	curve: Ti.UI.iOS.ANIMATION_CURVE_EASE_OUT,
 	duration: 500
 });
-var win = Titanium.UI.createWindow();
+var win = Titanium.UI.createWindow({
+	left: 0,
+	zIndex: 10
+});
 var win1 = Titanium.UI.createWindow({
     backgroundColor: 'white',
-    title: 'Facebook menu'
+    title: 'Facebook menu',
+    left: 0,
+	zIndex: 10
 });
 var nav = Titanium.UI.iPhone.createNavigationGroup({
    window: win1,
+   left: 0,
    width: Ti.Platform.displayCaps.platformWidth
 });
 var button = Ti.UI.createButton({
-	systemButton: Ti.UI.iPhone.SystemButton.ORGANIZE,
+	title: 'm',
 	left: 10,
 	width: 30,
 	height: 30,
 	top: 10
 });
-
-win1.addEventListener('swipe',function(e){
-	if( e.direction == 'left' ){
-		button.fireEvent('click');
+var touchStartX = 0;
+var touchStarted = false;
+win1.addEventListener('touchstart',function(e){
+	touchStartX = parseInt(e.x,10);
+});
+win1.addEventListener('touchend',function(e){
+	touchStarted = false;
+	if( win.left >= 140 ){
+		win.animate(animateLeft);
+		isToggled = true;
 	} else {
-		button.fireEvent('click');
+		win.animate(animateRight);
+		isToggled = false;
+	}
+});
+win1.addEventListener('touchmove',function(e){
+	var x 		= parseInt(e.globalPoint.x, 10);
+	var newLeft = x - touchStartX;
+	if( newLeft >= 0 ){
+		if( touchStarted ){
+			win.left	= newLeft;
+		}
+	}
+	// Minimum movement is 30
+	if( newLeft > 30 ){
+		touchStarted = true;
 	}
 });
 nav.add(button);
@@ -36,37 +77,13 @@ win.add(nav);
 win.open();
 
 
-// Facebook like menu window
-var menu	= Ti.UI.createWindow({
-	backgroundColor: 'red',
-	top: 0,
-	left: -250,
-	width: 250
-});
-var data = [{title:"Row 1"},{title:"Row 2"},{title:"Row 3"},{title:"Row 4"}];
-var tableView	= Ti.UI.createTableView({
-	data: data
-});
-menu.add(tableView);
-menu.open();
-
-
-
 var isToggled = false;
 button.addEventListener('click',function(e){
 	if( !isToggled ){
-		nav.animate(animateLeft);
-		menu.animate({
-			left: 0,
-			duration: 500
-		});
+		win.animate(animateLeft);
 		isToggled = true;
 	} else {
-		nav.animate(animateRight);
-		menu.animate({
-			left: -250,
-			duration: 500
-		});
+		win.animate(animateRight);
 		isToggled = false;
 	}
 });
